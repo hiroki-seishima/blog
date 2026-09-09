f1c8b4f3dd48de1e6f2a978b362836259cc9213d
hiroki-seishima <yuguiqingdao@gmail.com>
2026-09-10 06:45:44 +0900

    Post.javaとpostStatus.javaの編集

--- src/main/java/com/example/blog/entity/Post.java
+++ src/main/java/com/example/blog/entity/Post.java
@@ -1,28 +1,33 @@
 package com.example.blog.entity;
 
-import jakarta.persistence.*;
-import java.time.LocalDateTime;
+import jakarta.persistence.*;  //JPA（DBとJavaオブジェクトを対応させる仕組み）
+import java.time.LocalDateTime;  //日時を扱うためのJavaの標準クラス。
 
-
-@Entity
+//クラスレベル
+@Entity //このクラスはDBのテーブルに対応するデータの型ですよ」とSpringに伝えるアノテーション
 @Table(name="posts")  //DBテーブル名
 public class Post {
 
-//カラム
-	@Id //
-	@GeneratedValue(strategy = GenerationType.IDENTITY) //
+	//フィールドレベル
+	//idフィールド
+	@Id //主キー（そのレコードを一意に識別するもの）
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //idの値をDB側（MySQLなどのAUTO_INCREMENT機能）で自動生成するよう指定。自分でidを指定する必要はなくなります。
 	private Long id;  //Longは数値型の桁多め
-
-	@Column(nullable=false) // 
-	private String title;
 	
-	@Lob
-	private String content; //
+	//titleフィールド
+	@Column(nullable=false) // カラム（nullはダメ）
+	private String title; //記事タイトル
 	
-	@Enumerated(EnumType.STRING) //
-	private PostStatus status; //
-	private LocalDateTime createdAt; //作成日
+	//contentフィールド
+	@Lob // "Large OBject"の略。長い文章を保存するためのカラム型（DBだとTEXT型など）を使うよう指定　＜ー＞　@Basicもしくはかアノテーション不要
+	private String content; //記事内容
 	
+	//statusフィールド
+	@Enumerated(EnumType.STRING) //enum型を、DBには文字列として保存するよう指定。enumとは（列举型、Enumerationの略）。決まった選択肢の中からしか値を選べない型を作るためのJavaの機能。
+	private PostStatus status; //PostStatusはenum型　記事の状態（公開か非公開かとか）
+	private LocalDateTime createdAt; //作成日、投稿日時
+	
+	// getterとsetterはフィールド毎に必須
 	public Long getId() {
 		return id;
 	} //getterはこれが定型文
@@ -44,7 +49,7 @@ public class Post {
 	public PostStatus getStatus() {
 		return status;
 	}
-	public void setStatus(Poststatus status) {
+	public void setStatus(PostStatus status) {
 		this.status = status;
 	}
 	public LocalDateTime getCreatedAt() {
--- src/main/java/com/example/blog/entity/PostStatus.java
+++ src/main/java/com/example/blog/entity/PostStatus.java
@@ -1,6 +1,14 @@
 package com.example.blog.entity;
 
-public enum PostStatus { //
-	DRAFT, //
-	PUBLISHED //
+public enum PostStatus { //普通にStringだと自由に文字を入れられるため、タイポ（入力ミスや打ち間違いのこと）を避けるためにPostStatus型を設定。
+	DRAFT, //下書き状態
+	PUBLISHED //公開状態に変更
 }
+//enumは他にもある。必要に応じて作成が必要。enum１つにつき１ファイルで作成したほうが管理は楽(パッケージは揃えることは必須)
+//(例)
+//com.example.blog.entity
+//├── Post.java
+//├── PostStatus.java　->　enum
+//├── PostVisibility.java -> enum
+//├── User.java
+//└── UserRole.java -> enum
