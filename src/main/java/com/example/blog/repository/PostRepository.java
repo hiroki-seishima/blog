ff6a181a1e4a7d77320c848ff5b9b2d59708b6f4
清島裕貴 <seishimahiroki@kiyojimayukinoMacBook-Pro.local>
2026-09-11 02:57:57 +0900

    PostRepository.javaとPostService.javaを作成

--- src/main/java/com/example/blog/repository/PostRepository.java
+++ src/main/java/com/example/blog/repository/PostRepository.java
@@ -1,5 +1,30 @@
-package com.example.blog.repository;
+package com.example.blog.repository;//DB操作の手段だけを提供
 
-public class PostRepository {
+import com.example.blog.entity.Post; //Post.javaと連携
+import com.example.blog.entity.PostStatus; //PostStatus.javaと連携
+import org.springframework.data.jpa.repository.JpaRepository; //JpaRepositoryという、Spring Data JPAが用意している「DB操作の基本機能一式」を継承するためのアノテーション
+import java.util.List;  //リスト
 
+public interface PostRepository extends JpaRepository<Post, Long> {  //JpaRepositoryという、Spring Data JPAが用意している「DB操作の基本機能一式」を継承。また、<対象のEntity, そのEntityの主キーの型>
+	//この形は決まり
+
+	//これらが自動で反映
+	//postRepository.save(post);          // 保存・更新
+	//postRepository.findById(1L);        // idで1件検索
+	//postRepository.findAll();           // 全件取得
+	//postRepository.deleteById(1L);      // idで削除
+	//postRepository.count();             // 件数取得
+	List<Post> findByStatus(PostStatus status); //"公開済みの記事だけ取得したい"というブログ特有の要件に対応するためのメソッド
+	//SQLで言うと->SELECT * FROM posts WHERE status = ?
+	
+	//このような指示もあり
+//	findByTitle(String title);                          // タイトルで検索
+//	findByTitleContaining(String keyword);               // タイトルに部分一致
+//	findByCreatedAtAfter(LocalDateTime date);            // 指定日時より後の記事
+//	findByStatusAndTitleContaining(PostStatus status, String keyword); // 複数条件
+	
+	
+	
 }
+
+
--- src/main/java/com/example/blog/service/PostService.java
+++ src/main/java/com/example/blog/service/PostService.java
@@ -1,5 +1,34 @@
 package com.example.blog.service;
 
-public class PostService {
 
+import com.example.blog.entity.Post;
+import com.example.blog.entity.PostStatus;
+import com.example.blog.repository.PostRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime; //日時を扱うためのJavaの標準クラス。
+import java.util.List; //リスト
+
+//クラスレベルで設定
+@Service //ビジネスロック（実際の処理内容）を担当するクラス　これをつけることによりこのクラスのインスタンスを１つつくって管理してくれるようになる。->この仕組みをDI(依存性注入)コンテナで管理されるという
+public class PostService {
+	//フィイールド
+	private final PostRepository postRepository;
+	//コンストラクタで受け取る
+	public PostService(PostRepository postRepository) {
+		this.postRepository = postRepository;
+	}
+
+	public Post create(String title,String content) {
+		Post post = new Post(); //空のPostオブジェクトを作る
+		post.setTitle(title); //タイトルを引数で受け取った値で埋める
+		post.setContent(content); //本文を、引数で受け取った値で埋める
+		post.setStatus(PostStatus.DRAFT); //ステータスは新規作成時は自動的にDRAFT（下書き）にする
+		post.setCreatedAt(LocalDateTime.now()); //作成日時を現在時刻で記録
+		return postRepository.save(post);  //DBに保存
+	}
+	
+	public List<Post> funndPublished(){ //PostRepositoryのfindByStatusメソッドを使って、PUBLISHED(公開済み)の記事だけを取得。公開済みの記事だけを一覧表示したいという要件を表現
+		return postRepository.findByStatus(PostStatus.PUBLISHED); 
+	}
 }
